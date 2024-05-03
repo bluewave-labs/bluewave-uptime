22f360b6360fe809c1cc1bc457d203a40cd250c8
veyselboybay <veysel.boybay@outlook.com>
2024-05-03 12:29:37 -0400

    Created a boilerplate project for client and server environments

---
+++ Server/configs/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+const PORT = process.env.PORT || 5000
+const connectDbAndRunServer = async(app) => {    
+    await mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+        console.log("DB connected")
+        // run server
+        app.listen(PORT, () => {
+            console.log(`server started on port:${PORT}`)
+        })
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+
+module.exports = { connectDbAndRunServer }
