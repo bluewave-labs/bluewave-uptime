79ca5021bebf46b8c116998abe3d7bdcdac388c8
M M <cosmicray23@hotmail.com>
2024-05-05 15:44:48 -0700

    Removed unused React import.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react'
 import './App.css'
-import Button from '@mui/material/Button';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
--- Client/src/Dropdown.jsx
+++ Client/src/Dropdown.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types'; 
 
 import Autocomplete from '@mui/material/Autocomplete';
