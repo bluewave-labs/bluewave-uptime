6533dd1132e1d12d6a7469c1016a687d1248e7de
Alex Holliday <ajhollid@gmail.com>
2024-05-06 12:27:48 -0700

    Added button examples to home page

--- Client/src/Pages/Home/index.jsx
+++ Client/src/Pages/Home/index.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import DropdownTeamMember from '../../Components/DropdownTeamMember'
-import './index.css'
-
+import React from "react";
+import DropdownTeamMember from "../../Components/DropdownTeamMember";
+import "./index.css";
+import Button from "../../Components/Button";
 
 const Home = () => {
-    return (
-        <>
-            <div>Home</div>
-            <DropdownTeamMember />
-        </>
-    )
-}
+  return (
+    <>
+      <div>Home</div>
+      <DropdownTeamMember />
+      <div style={{ display: "flex", flexDirection: "flex-column" }}>
+        <div>
+          <Button level="primary" label="Primary" />
+          <Button level="secondary" label="Secondary" />
+          <Button level="tertiary" label="Tertiary" />
+          <Button level="error" label="Error" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
