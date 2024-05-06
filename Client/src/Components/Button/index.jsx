f13b64a496537e52a9949fe98576ccfcfc181a68
Alex Holliday <ajhollid@gmail.com>
2024-05-06 13:39:58 -0700

    Added start and end icon props to buttons to allow for images, made prop type for level more specific, added onClick prop so a function can be passed to onClick

--- Client/src/Components/Button/index.jsx
+++ Client/src/Components/Button/index.jsx
@@ -24,22 +24,35 @@ const levelConfig = {
  * @typedef {Object} Props
  * @property {'primary' | 'secondary' | 'tertiary' | 'error'} level - The level of the button
  * @property {string} label - The label of the button
+ * @property {React.ReactNode} [startIcon] - Icon prepended to the label
+ * @property {React.ReactNode} [endIcon] - Icon appended to the button label
  * @property {boolean} [disabled] - Whether the button is disabled
+ * @property {(event: React.MouseEvent<HTMLButtonElement>) => void} [onClick] - The click handler of the button
  */
 
-const Button = ({ level, label, disabled }) => {
+const Button = ({ level, label, disabled, startIcon, endIcon, onClick }) => {
   const { variant, color } = levelConfig[level];
   return (
-    <MuiButton variant={variant} color={color} disabled={disabled}>
+    <MuiButton
+      variant={variant}
+      color={color}
+      disabled={disabled}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      onClick={onClick}
+    >
       {label}
     </MuiButton>
   );
 };
 
 Button.propTypes = {
-  level: PropTypes.string.isRequired,
+  level: PropTypes.oneOf(["primary", "secondary", "tertiary", "error"]),
   label: PropTypes.string.isRequired,
+  startIcon: PropTypes.node,
+  endIcon: PropTypes.node,
   disabled: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- Client/src/Pages/Home/index.jsx
+++ Client/src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DropdownTeamMember from "../../Components/DropdownTeamMember";
 import "./index.css";
 import Button from "../../Components/Button";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Home = () => {
   return (
@@ -13,10 +14,24 @@ const Home = () => {
       >
         <h4>Buttons</h4>
         <div>
-          <Button level="primary" label="Primary" />
-          <Button level="secondary" label="Secondary" />
-          <Button level="tertiary" label="Tertiary" />
-          <Button level="error" label="Error" />
+          <Button
+            level="primary"
+            label="Primary"
+            onClick={() => {
+              alert("Primary");
+            }}
+          />
+          <Button
+            level="secondary"
+            label="Secondary"
+            onClick={() => alert("Secondary")}
+          />
+          <Button
+            level="tertiary"
+            label="Tertiary"
+            onClick={() => alert("Tertiary")}
+          />
+          <Button level="error" label="Error" onClick={() => alert("Error")} />
         </div>
         <h4>Disabled Buttons</h4>
         <div>
@@ -24,6 +39,19 @@ const Home = () => {
           <Button level="secondary" label="Secondary" disabled />
           <Button level="tertiary" label="Tertiary" disabled />
           <Button level="error" label="Error" disabled />
+        </div>
+        <h4>Image Button</h4>
+        <div>
+          <Button
+            level="tertiary"
+            label="Configure"
+            startIcon={<SettingsIcon />}
+          />
+          <Button
+            level="tertiary"
+            label="Configure"
+            endIcon={<SettingsIcon />}
+          />
         </div>
       </div>
     </>
