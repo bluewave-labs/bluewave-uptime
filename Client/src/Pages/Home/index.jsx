d40920c43a95a63a25e07d56bd4e396cb0eff20d
Veysel <66980512+veyselboybay@users.noreply.github.com>
2024-05-07 14:40:33 -0400

    Revert "Added start and end icon props to buttons to allow for images, made p…"

--- Client/src/Pages/Home/index.jsx
+++ Client/src/Pages/Home/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DropdownTeamMember from "../../Components/DropdownTeamMember";
 import "./index.css";
 import Button from "../../Components/Button";
-import SettingsIcon from "@mui/icons-material/Settings";
 
 const Home = () => {
   return (
@@ -14,24 +13,10 @@ const Home = () => {
       >
         <h4>Buttons</h4>
         <div>
-          <Button
-            level="primary"
-            label="Primary"
-            onClick={() => {
-              alert("Primary");
-            }}
-          />
-          <Button
-            level="secondary"
-            label="Secondary"
-            onClick={() => alert("Secondary")}
-          />
-          <Button
-            level="tertiary"
-            label="Tertiary"
-            onClick={() => alert("Tertiary")}
-          />
-          <Button level="error" label="Error" onClick={() => alert("Error")} />
+          <Button level="primary" label="Primary" />
+          <Button level="secondary" label="Secondary" />
+          <Button level="tertiary" label="Tertiary" />
+          <Button level="error" label="Error" />
         </div>
         <h4>Disabled Buttons</h4>
         <div>
@@ -39,19 +24,6 @@ const Home = () => {
           <Button level="secondary" label="Secondary" disabled />
           <Button level="tertiary" label="Tertiary" disabled />
           <Button level="error" label="Error" disabled />
-        </div>
-        <h4>Image Button</h4>
-        <div>
-          <Button
-            level="tertiary"
-            label="Configure"
-            startIcon={<SettingsIcon />}
-          />
-          <Button
-            level="tertiary"
-            label="Configure"
-            endIcon={<SettingsIcon />}
-          />
         </div>
       </div>
     </>
