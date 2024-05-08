bac424c45d82762c7410e093573813618f0cc0a8
MuhammadKhalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-05-08 12:13:09 +0330

    Before switching to charts

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import PlayGround from "./screens/PlayGround/PlayGround";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <PlayGround />
-      </div>
+      <div></div>
     </>
   );
 }
