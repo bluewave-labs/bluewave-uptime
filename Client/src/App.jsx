13a458cf4368ddbffdd19b232e41c84d9e78acbd
Alex Holliday <ajhollid@gmail.com>
2024-05-08 14:50:11 -0700

    Add DataGrid

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,23 @@
-
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import NotFound from './Pages/NotFound'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import HomeLayout from './Layouts/HomeLayout'
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NotFound from "./Pages/NotFound";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import HomeLayout from "./Layouts/HomeLayout";
+import Demo from "./Pages/Demo/Demo";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<HomeLayout />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
+        <Route exact path="/" element={<HomeLayout />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/demo" element={<Demo />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
