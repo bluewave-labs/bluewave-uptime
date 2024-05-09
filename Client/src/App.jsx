316fc44aeba2548615445c47bc1aea76d834b6b7
MuhammadKhalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-05-09 11:28:58 +0330

    reviewing popup modals

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,7 @@
-import PlayGroundPopupModals from "./screens/PlayGround/PlayGround-Popup-Modals";
-
 function App() {
   return (
     <>
-      <div>
-        <PlayGroundPopupModals />
-      </div>
+      <div></div>
     </>
   );
 }
