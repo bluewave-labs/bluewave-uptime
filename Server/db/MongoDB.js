4e12778f001b0e809228fe11a74a7e82ae6aba1c
Alex Holliday <ajhollid@gmail.com>
2024-05-13 15:22:35 -0700

    Remove seed data, not required

--- Server/db/MongoDB.js
+++ Server/db/MongoDB.js
@@ -1,41 +1,10 @@
 const Monitor = require("../models/Monitor");
 const mongoose = require("mongoose");
 
-const seedDatabase = async () => {
-  const FAKE_MONITOR_DATA = [];
-
-  for (let i = 0; i < 10; i++) {
-    FAKE_MONITOR_DATA.push(
-      new Monitor({
-        userId: i % 2 === 0 ? 1 : 2,
-        name: `Monitor ${i}`,
-        description: `Description for Monitor ${i}`,
-        url: `https://monitor${i}.com`,
-        isActive: true,
-        interval: 60000,
-        updated_at: new Date(),
-        created_at: new Date(),
-      })
-    );
-  }
-
-  for (const monitor of FAKE_MONITOR_DATA) {
-    await monitor.save();
-  }
-};
-
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log("Connected to MongoDB");
-
-    const monitors = await Monitor.find();
-    if (monitors.length === 0) {
-      console.log("Seeding database with fake data");
-      await seedDatabase();
-    } else {
-      console.log("Database already seeded");
-    }
   } catch (error) {
     console.error("Failed to connect to MongoDB");
     throw error;
