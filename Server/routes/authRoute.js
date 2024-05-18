3a94abb6490dbeb07fa8b1a7287024eb86841b3f
Mohammad Khalilzadeh <140876993+MuhammadKhalilzadeh@users.noreply.github.com>
2024-05-18 09:04:41 +0330

    Delete Server directory

--- Server/routes/authRoute.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { registerController } = require('../controllers/authController')
-
-router.post('/register', registerController)
-
-module.exports = router;
--- Server/routes/monitorRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const monitorController = require("../controllers/monitorController");
-router.get("/", monitorController.getAllMonitors);
-router.get("/:monitorId", monitorController.getMonitorById);
-router.get("/user/:userId", monitorController.getMonitorsByUserId);
-
-module.exports = router;
