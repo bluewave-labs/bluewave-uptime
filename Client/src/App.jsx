d384de84d6b3881435806e0b0cdb3180b2dee5e3
MuhammadKhalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-05-22 17:46:58 +0330

    Login page ui implemented

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,23 @@
-// import { Routes, Route } from "react-router-dom";
-// import "./App.css";
-// import NotFound from "./Pages/NotFound";
-// import Login from "./Pages/Login";
-// import Register from "./Pages/Register";
-// import HomeLayout from "./Layouts/HomeLayout";
-// import Demo from "./Pages/Demo/Demo";
-// import PlayGround from "./Pages/PlayGround/PlayGround";
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NotFound from "./Pages/NotFound";
 import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import HomeLayout from "./Layouts/HomeLayout";
+import Demo from "./Pages/Demo/Demo";
+import PlayGround from "./Pages/PlayGround/PlayGround";
 
 function App() {
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route exact path="/" element={<HomeLayout />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/demo" element={<Demo />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/playground" element={<PlayGround />} />
-      </Routes> */}
-      <Login />
+      </Routes>
     </>
   );
 }
