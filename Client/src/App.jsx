90b5d894c8fc23a2926808f57a3c0be0f65141c2
MuhammadKhalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-05-22 17:41:24 +0330

    Login page ui implemented

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,23 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import NotFound from "./Pages/NotFound";
+// import { Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import NotFound from "./Pages/NotFound";
+// import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
+// import HomeLayout from "./Layouts/HomeLayout";
+// import Demo from "./Pages/Demo/Demo";
+// import PlayGround from "./Pages/PlayGround/PlayGround";
+
 import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import HomeLayout from "./Layouts/HomeLayout";
-import Demo from "./Pages/Demo/Demo";
-import PlayGround from "./Pages/PlayGround/PlayGround";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route exact path="/" element={<HomeLayout />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/demo" element={<Demo />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/playground" element={<PlayGround />} />
-      </Routes>
+      </Routes> */}
+      <Login />
     </>
   );
 }
---
+++ Client/src/Components/TextFields/Password/PasswordTextField.jsx
@@ -0,0 +1,89 @@
+import "./passwordTextField.css";
+import React, { useState } from "react";
+import { InputAdornment, TextField } from "@mui/material";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import { useTheme } from "@mui/material";
+
+/**
+ * @component
+ * @param {Object} props
+ * @param {string} props.id - Unique ID for the text field (optional)
+ * @param {string} props.label - The label text displayed above the text field (optional)
+ * @param {"standard" | "outlined" | "filled"} props.variant - The variant of the text field (e.g., "outlined") (optional)
+ * @param {string} props.placeholder - The placeholder text displayed within the text field (optional)
+ * @param {"error" | "help"} [props.icon] - The type of icon to display (error or help) (optional)
+ * @param {string} props.helperText - The helper text displayed below the text field (optional)
+ * @param {boolean} props.error - A flag indicating if the text field has an error (required)
+ * @returns {JSX.Element} - Renders the password text field component with dynamic icon display
+ */
+const PasswordTextField = ({
+  id,
+  label,
+  variant,
+  placeholder,
+  icon,
+  helperText,
+  error,
+}) => {
+  const theme = useTheme();
+
+  const fontLookup = {
+    default: theme.font.default.font,
+  };
+
+  const fontFamily = fontLookup["default"];
+
+  const [showIcon, setShowIcon] = useState(false);
+
+  const handleMouseEnter = () => setShowIcon(true);
+  const handleMouseLeave = () => setShowIcon(false);
+
+  return (
+    <div style={{ fontFamily: fontFamily }}>
+      <div className="password-text-field-title">Password</div>
+      <div className="password-text-field">
+        <TextField
+          type="password"
+          error={error}
+          className="password-text-field-input"
+          id={id}
+          label={label}
+          variant={variant}
+          placeholder={placeholder}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <div className="icon-holder">
+                  {error && showIcon && (
+                    <ErrorOutlineIcon
+                      className="text-field-icon"
+                      style={{ fill: "red", width: "16px", height: "16px" }}
+                    />
+                  )}
+                  {error && !showIcon && (
+                    <ErrorOutlineIcon
+                      className="text-field-icon"
+                      style={{ fill: "red", width: "16px", height: "16px" }}
+                    />
+                  )}
+                  {!error && showIcon && (
+                    <HelpOutlineIcon
+                      className="text-field-icon"
+                      style={{ width: "16px", height: "16px" }}
+                    />
+                  )}
+                </div>
+              </InputAdornment>
+            ),
+          }}
+          helperText={helperText}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PasswordTextField;
