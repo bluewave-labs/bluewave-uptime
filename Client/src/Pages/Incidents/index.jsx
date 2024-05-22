3537f3fa194169f6e6f08b28dc3f9d7bbf87a2ef
veyselboybay <veysel.boybay@outlook.com>
2024-05-22 17:14:34 -0400

    Added subroutes to homelayout and created basic respective components, and removed demo components from home page #45

---
+++ Client/src/Pages/Incidents/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Incidents = () => {
+    return (
+        <div>Incidents</div>
+    )
+}
+
+export default Incidents
---
+++ Client/src/Pages/Settings/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Settings = () => {
+    return (
+        <div>Settings</div>
+    )
+}
+
+export default Settings
