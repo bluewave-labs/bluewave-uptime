d24b5d8ada832342116bdb95f1477354a0697675
M M <cosmicray23@hotmail.com>
2024-05-27 23:11:23 -0700

    Addressed comments and fixed the issue with the import statement.

--- Client/src/Layouts/HomeLayout/index.jsx
+++ Client/src/Layouts/HomeLayout/index.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 import Home from '../../Pages/Home'
 import './index.css'
 import ChartsOverviewDemo from '../../Components/DashboardProgressBars'
-import DashboardSettings from '../../Components/DashboardSettings'
+import DashboardSettings from '../../Components/DashboardSettingsIcon'
 
 
 const HomeLayout = () => {
