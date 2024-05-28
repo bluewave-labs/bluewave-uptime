7e9f8d9d290f39ec9122f74940be493d02d1b2a7
Alex Holliday <ajhollid@gmail.com>
2024-05-28 11:34:25 -0700

    Implemented routes

--- Server/controllers/alertController.js
+++ Server/controllers/alertController.js
@@ -1,9 +1,28 @@
-const createAlert = (req, res, next) => {};
-const getAlertsByUserId = (req, res, next) => {};
-const getAlertsByMonitorId = (req, res, next) => {};
-const getAlertById = (req, res, next) => {};
-const editAlert = (req, res, next) => {};
-const deleteAlert = (req, res, next) => {};
+const express = require("express");
+const {} = require("../validation/joi");
+const logger = require("../utils/logger");
+require("dotenv").config();
+
+const SERVICE_NAME = "alerts";
+
+const createAlert = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create Alert" });
+};
+const getAlertsByUserId = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Get Alerts By UserID" });
+};
+const getAlertsByMonitorId = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Get Alerts By MonitorID" });
+};
+const getAlertById = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Get Alert By alertID" });
+};
+const editAlert = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Edit alert" });
+};
+const deleteAlert = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Delete alert" });
+};
 
 module.exports = {
   createAlert,
