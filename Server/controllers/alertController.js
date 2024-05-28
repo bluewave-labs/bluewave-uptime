7430a6eb48e835ac0ebfdeeb2f9b70f2b0ea33e2
Alex Holliday <ajhollid@gmail.com>
2024-05-28 11:17:03 -0700

    Started work on alert routes and controller

---
+++ Server/controllers/alertController.js
@@ -0,0 +1,15 @@
+const createAlert = (req, res, next) => {};
+const getAlertsByUserId = (req, res, next) => {};
+const getAlertsByMonitorId = (req, res, next) => {};
+const getAlertById = (req, res, next) => {};
+const editAlert = (req, res, next) => {};
+const deleteAlert = (req, res, next) => {};
+
+module.exports = {
+  createAlert,
+  getAlertsByUserId,
+  getAlertsByMonitorId,
+  getAlertById,
+  editAlert,
+  deleteAlert,
+};
