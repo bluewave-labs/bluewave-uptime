fc65ce17ae47946255a8c5a52634b6840d15a1b6
M M <cosmicray23@hotmail.com>
2024-06-01 14:54:29 -0700

    Added the support icon at the bottom of the sidebar.

--- Client/src/Components/DashboardSidebar/index.jsx
+++ Client/src/Components/DashboardSidebar/index.jsx
@@ -1,9 +1,11 @@
 import DashboardMenu from '../../Components/DashboardMenu';
+import SupportIcon from '@mui/icons-material/Support'; 
+import SvgIcon from '@mui/material/SvgIcon';
 import './index.css';
 
 /**
  * @component
- * DashboardSidebar component serves as a sidebar containing the DashboardMenu.
+ * DashboardSidebar component serves as a sidebar containing the DashboardMenu and a Support icon at the bottom.
  * 
  * @returns {JSX.Element} The JSX element representing the DashboardSidebar component.
  */
@@ -11,7 +13,13 @@ import './index.css';
 function DashboardSidebar() {
   return (
     <div className="dashboard-sidebar">
-      <DashboardMenu />
+      <div className="menu-container">
+        <DashboardMenu />
+      </div>
+      <div className="support-container">
+        <SvgIcon component={SupportIcon} />
+        <span className="support-text">Support</span>
+      </div>
     </div>
   );
 }
