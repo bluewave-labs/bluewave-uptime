ef6263ab21ca6eb8eb4554387ce8d49408159a47
M M <cosmicray23@hotmail.com>
2024-06-06 17:39:15 -0700

    Encode user input from text fields into a JSON object.

--- Client/src/Pages/Login/index.jsx
+++ Client/src/Pages/Login/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./index.css";
 import BackgroundPattern from "../../Components/BackgroundPattern/BackgroundPattern";
 import Logomark from "../../assets/Images/Logomark.png";
