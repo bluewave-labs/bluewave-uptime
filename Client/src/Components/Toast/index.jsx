2de2b359afc0766e654038c148e7855af791383f
M M <cosmicray23@hotmail.com>
2024-06-15 19:06:51 -0700

    Refactored Toast Component into a custom React Toastify component.

--- Client/src/Components/Toast/index.jsx
+++ Client/src/Components/Toast/index.jsx
@@ -1,63 +1,30 @@
-import { useState, useEffect } from "react";
 import ComplexAlert from "../../Components/Icons/ComplexAlert/ComplexAlert";
-import "./index.css"; // Import your CSS file for toast styles
+import AnnouncementsDualButtonWithIcon from "../../Components/Announcements/AnnouncementsDualButtonWithIcon/AnnouncementsDualButtonWithIcon";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+import "./index.css";
 
-const Toast = () => {
-  const [showToast, setShowToast] = useState(false);
-  const [progress, setProgress] = useState(100); // Start with full progress
-
-  useEffect(() => {
-    let timer;
-    if (showToast) {
-      timer = setTimeout(() => {
-        setShowToast(false);
-      }, 5000); // 5000 milliseconds = 5 seconds
-
-      // Interval to update progress bar
-      const interval = setInterval(() => {
-        setProgress((prevProgress) => prevProgress - (100 / 5000 * 100)); // Decrease progress based on time
-      }, 100); // Update every 100 milliseconds
-
-      return () => {
-        clearTimeout(timer);
-        clearInterval(interval);
-      };
-    }
-  }, [showToast]);
-
-  const toggleToast = () => {
-    setShowToast(!showToast);
-    setProgress(100); // Reset progress when showing toast
+const ToastComponent = () => {
+  const displayMsg = () => {
+    toast(
+      <AnnouncementsDualButtonWithIcon
+        icon={<ComplexAlert theme="red" />}
+        subject="There was a problem with that action"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum dolor."
+        esc="Dismiss"
+        primary="Learn more"
+        dismiss={() => toast.dismiss()}
+      />,
+      { closeButton: false }
+    );
   };
 
   return (
-    <div className="toast-container">
-      <button onClick={toggleToast}>Show Toast</button>
-      {showToast && (
-        <div className="toast">
-          <span className="close-button" onClick={toggleToast}>&times;</span>
-          <div className="icon">
-            {<ComplexAlert theme="red" />}
-          </div>
-          <div className="toast-content">
-            <div className="announcement-content">
-              <div className="announcement-content-subject">
-                There was a problem with that action
-              </div>
-              <div className="announcement-content-body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum dolor.
-              </div>
-              <div className="announcement-content-controllers">
-                <div className="controllers-button-esc" onClick={toggleToast}>Dismiss</div>
-                <div className="controllers-button-primary">View changes</div>
-              </div>
-            </div>
-          </div>
-          <div className="toast-progress" style={{ width: `${progress}%` }}></div>
-        </div>
-      )}
+    <div>
+      <button onClick={displayMsg}>Click me</button>
+      <ToastContainer className="Toastify__toast-container" />
     </div>
   );
 };
 
-export default Toast;
+export default ToastComponent;
