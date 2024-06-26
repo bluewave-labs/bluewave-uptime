0815cb69a773d5a83643b3d09afeeaf07b6ef073
terryjung0244 <terryjung0244@gmail.com>
2024-06-26 11:03:00 -0400

    Created a Container and Buttons using MUI.

--- Client/src/Components/TerryComponent/index.jsx
+++ Client/src/Components/TerryComponent/index.jsx
@@ -1,7 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Button, Container } from "@mui/material";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Container style={{ marginTop: "100px", marginLeft: "70px" }}>
+        <Button variant="contained">Feature Alert</Button>
+        <Button
+          style={{ margin: "10px 20px 10px 20px" }}
+          className="button"
+          variant="contained"
+        >
+          Problem Alert
+        </Button>
+        <Button variant="contained">Update Alert</Button>
+      </Container>
+    </div>
+  );
 };
 
 export default index;
