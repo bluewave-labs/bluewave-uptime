8df1a33e4bf59d2271e606b0d449241c1d6b91f8
terryjung0244 <terryjung0244@gmail.com>
2024-06-26 11:11:10 -0400

    Added useState for multiple states and created handleOpenAlertMessage function with parameters.

--- Client/src/Components/TerryComponent/index.jsx
+++ Client/src/Components/TerryComponent/index.jsx
@@ -1,20 +1,72 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container } from "@mui/material";
 
 const index = () => {
+  const [notification, setNotification] = useState({
+    open: false,
+    title: "",
+    message: "",
+    type: "",
+    alertMessageType: "",
+    actionText: "",
+  });
+
+  const handleOpenAlertMessage = (type, title, message, actionText) => {
+    setNotification({
+      ...notification,
+      open: true,
+      title,
+      type,
+      message,
+      actionText,
+    });
+  };
+
   return (
     <div>
       <Container style={{ marginTop: "100px", marginLeft: "70px" }}>
-        <Button variant="contained">Feature Alert</Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            handleOpenAlertMessage(
+              "release",
+              "We've just released a new feature",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Aliquid pariatur, ipsum dolor.",
+              "View changes"
+            )
+          }
+        >
+          Feature Alert
+        </Button>
         <Button
           style={{ margin: "10px 20px 10px 20px" }}
           className="button"
           variant="contained"
+          onClick={() =>
+            handleOpenAlertMessage(
+              "problem",
+              "There was a problem with that action",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Aliquid pariatur, ipsum dolor.",
+              "Learn more"
+            )
+          }
         >
           Problem Alert
         </Button>
-        <Button variant="contained">Update Alert</Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            handleOpenAlertMessage(
+              "update",
+              "Successfully updated profile",
+              "Lorem ipsum dolor sit amet consectetur adipiscing elit. Aliquid pariatur, ipsum dolor.",
+              "View changes"
+            )
+          }
+        >
+          Update Alert
+        </Button>
       </Container>
     </div>
   );
