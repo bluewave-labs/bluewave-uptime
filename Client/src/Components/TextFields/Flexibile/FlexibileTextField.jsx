df27ae8976369dda5bad3291f99d9e2421e29f6c
Daniel Cojocea <danielcojocea28@gmail.com>
2024-06-28 20:09:34 -0400

    Added proptypes and jsdocs

--- Client/src/Components/TextFields/Flexibile/FlexibileTextField.jsx
+++ Client/src/Components/TextFields/Flexibile/FlexibileTextField.jsx
@@ -1,7 +1,19 @@
+import PropTypes from "prop-types";
 import { Stack, Typography } from "@mui/material";
 import "./flexibileTextField.css";
-import React from "react";
 import { useTheme } from "@emotion/react";
+
+/**
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The ID of the input field.
+ * @param {string} props.title - The title or label for the input field.
+ * @param {string} props.type - The type of the input field (e.g., 'text', 'password').
+ * @param {string} props.placeholder - The placeholder text for the input field.
+ * @param {string} props.value - The current value of the input field.
+ * @param {function} props.onChange - The function to call when the input field value changes.
+ * @param {string} [props.error] - Optional. Error message to display if there's an error with the input.
+ * @returns {JSX.Element} JSX Element representing the flexible text input field.
+ */
 
 const FlexibileTextField = ({
   id,
@@ -32,6 +44,16 @@ const FlexibileTextField = ({
       )}
     </Stack>
   );
+};
+
+FlexibileTextField.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string,
 };
 
 export default FlexibileTextField;
