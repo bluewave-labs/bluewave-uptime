e4c29e77aa19d4b84b324686490c0d7293f618f8
M M <cosmicray23@hotmail.com>
2024-06-28 18:00:09 -0700

    quick fix for eslint.

--- Client/src/Pages/Incidents/index.jsx
+++ Client/src/Pages/Incidents/index.jsx
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { ColoredLabel, StatusLabel } from "../../Components/Label/";
+import StatusLabel from "../../Components/Label/";
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
