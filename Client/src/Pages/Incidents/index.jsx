c8cdcd88a61698d102bddf218e7aa88e11fedf3b
terryjung0244 <terryjung0244@gmail.com>
2024-07-01 22:30:57 -0400

    Issue 225: Add basic structure for Incidents Component

--- Client/src/Pages/Incidents/index.jsx
+++ Client/src/Pages/Incidents/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent, Box, Typography, Button } from "@mui/material";
 
 const Incidents = () => {
-    return (
-        <div>Incidents</div>
-    )
-}
+  return (
+    <Card>
+      <CardContent>
+        <Box />
+        <Typography>Create a maintenance window to</Typography>
+      </CardContent>
+      <Box>
+        <Button>Let&apos;s create your maintenance window</Button>
+      </Box>
+    </Card>
+  );
+};
 
-export default Incidents
+export default Incidents;
