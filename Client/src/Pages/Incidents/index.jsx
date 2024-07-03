7d27a72f628394b4b1f484471714a7c9f3bbb545
terryjung0244 <terryjung0244@gmail.com>
2024-07-02 21:44:03 -0400

    Issue 225: update maintenance checklist component structure

--- Client/src/Pages/Incidents/index.jsx
+++ Client/src/Pages/Incidents/index.jsx
@@ -1,51 +1,37 @@
 import React from "react";
 import "./index.css";
-import {
-  Card,
-  CardContent,
-  Box,
-  Typography,
-  Button,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { Button } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 const Incidents = () => {
   const maintenanceItems = [
-    "Mark your maintenance periods",
-    "Eliminate any misunderstandings",
-    "Stop sending alerts in maintenance windows",
+    { id: 1, text: "Mark your maintenance periods" },
+    { id: 2, text: "Eliminate any misunderstandings" },
+    { id: 3, text: "Stop sending alerts in maintenance windows" },
   ];
 
   return (
-    <Card>
-      <CardContent>
-        <Box />
-        <Typography>Create a maintenance window to</Typography>
-        <List>
-          {maintenanceItems.map((item, index) => {
-            <ListItem key={index}>
-              <ListItemIcon>
-                <CheckCircleOutlineIcon color="primary" />
-              </ListItemIcon>
-              <ListItemText primary={item} />
-            </ListItem>;
-          })}
-        </List>
-      </CardContent>
-      <Box>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ textTransform: "none" }}
-        >
-          Let&apos;s create your maintenance window
-        </Button>
-      </Box>
-    </Card>
+    <div className="maintenance-checklist-main">
+      <div className="maintenance-image"></div>
+      <div className="maintenance-title">Create a maintenance window to</div>
+      <div>
+        {maintenanceItems.map((item) => (
+          <div key={item.id} className="checklist-item">
+            <CheckCircleOutlineIcon color="primary" />
+            <div className="checklist-item-text">{item.text}</div>
+          </div>
+        ))}
+      </div>
+
+      <Button
+        variant="contained"
+        color="primary"
+        className="maintenance-checklist-button"
+        sx={{ textTransform: "none" }}
+      >
+        Let&apos;s create your maintenance window
+      </Button>
+    </div>
   );
 };
 
