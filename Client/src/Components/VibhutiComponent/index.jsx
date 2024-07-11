04c57ff29c91deb1029b7c241776403062a71b06
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-07-11 14:38:42 -0400

    First Component
    Added propTypes and documentation for the component.

--- Client/src/Components/VibhutiComponent/index.jsx
+++ Client/src/Components/VibhutiComponent/index.jsx
@@ -3,12 +3,18 @@ import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-
-function VibhutiComponent() {
+import PropTypes from "prop-types";
+/**
+ * @component
+ * @param {'success' | 'info' | 'warning' | 'error'} props.severity - epresenting different states with corresponding icon and color combinations for each
+ * @param {string} props.title - Title of the announcement
+ * @param {string} props.message - message in your announcement
+ */
+function FirstComponent({ severity, title, message }) {
   return (
-    <Alert severity="info" onClose={() => {}}>
-      <AlertTitle>We've just released a new feature</AlertTitle>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    <Alert severity={severity} onClose={() => {}}>
+      <AlertTitle>{title}</AlertTitle>
+      {message}
       <Stack spacing={0} direction="row">
         <Button color="secondary" variant="text">
           Dismiss
@@ -20,4 +26,9 @@ function VibhutiComponent() {
     </Alert>
   );
 }
-export default VibhutiComponent;
+FirstComponent.propTypes = {
+  severity: PropTypes.string,
+  title: PropTypes.string,
+  message: PropTypes.string,
+};
+export default FirstComponent;
