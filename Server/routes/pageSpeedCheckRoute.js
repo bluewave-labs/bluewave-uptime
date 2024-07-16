cebab6669d8fa0c20c02e6dc3b654059441f6260
M M <cosmicray23@hotmail.com>
2024-07-15 22:14:09 -0700

    Added docs.

--- Server/routes/pageSpeedCheckRoute.js
+++ Server/routes/pageSpeedCheckRoute.js
@@ -3,18 +3,42 @@ const pageSpeedCheckController = require("../controllers/pageSpeedCheckControlle
 const { verifyOwnership } = require("../middleware/verifyOwnership");
 const PageSpeedCheck = require("../models/PageSpeedCheck");
 
+/**
+ * @route POST /:monitorId
+ * @description Create a new PageSpeedCheck for a monitor
+ * @access Private
+ * @param {string} monitorId - The ID of the monitor
+ * @middleware verifyOwnership - Ensures the user owns the monitor
+ * @controller pageSpeedCheckController.createCheck
+ */
 router.post(
   "/:monitorId",
   verifyOwnership(PageSpeedCheck, "monitorId"),
   pageSpeedCheckController.createCheck
 );
 
+/**
+ * @route GET /:monitorId
+ * @description Get all PageSpeedChecks for a monitor
+ * @access Private
+ * @param {string} monitorId - The ID of the monitor
+ * @middleware verifyOwnership - Ensures the user owns the monitor
+ * @controller pageSpeedCheckController.getChecks
+ */
 router.get(
   "/:monitorId",
   verifyOwnership(PageSpeedCheck, "monitorId"),
   pageSpeedCheckController.getChecks
 );
 
+/**
+ * @route POST /delete/:monitorId
+ * @description Delete all PageSpeedChecks for a monitor
+ * @access Private
+ * @param {string} monitorId - The ID of the monitor
+ * @middleware verifyOwnership - Ensures the user owns the monitor
+ * @controller pageSpeedCheckController.deleteChecks
+ */
 router.post(
   "/delete/:monitorId",
   verifyOwnership(PageSpeedCheck, "monitorId"),
