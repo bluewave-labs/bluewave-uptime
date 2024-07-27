50eaf820a6165b62c85c045e1ca5f466b1a99dcd
Daniel Cojocea <danielcojocea28@gmail.com>
2024-07-26 21:45:39 -0400

    Added basic styling

--- Client/src/Pages/PageSpeed/CreatePageSpeed/index.jsx
+++ Client/src/Pages/PageSpeed/CreatePageSpeed/index.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { useNavigate } from "react-router";
 import Field from "../../../Components/Inputs/Field";
 import Select from "../../../Components/Inputs/Select";
+import Button from "../../../Components/Button";
 const CreatePageSpeed = () => {
   const theme = useTheme();
   const navigate = useNavigate();
@@ -30,7 +31,11 @@ const CreatePageSpeed = () => {
           <IconButton
             aria-label="close modal"
             onClick={() => navigate("/page-speed")}
-            sx={{ p: "2px", mr: "-2px", "&:focus": { outline: "none" } }}
+            sx={{
+              p: "2px",
+              mr: "-2px",
+              "&:focus": { outline: "none" },
+            }}
           >
             <CloseRoundedIcon />
           </IconButton>
@@ -61,6 +66,19 @@ const CreatePageSpeed = () => {
             value={1}
             onChange={() => console.log("disabled")}
           />
+        </Stack>
+        <Stack gap={theme.gap.small}>
+          <Typography component="h2">Incidents notifications</Typography>
+          <Typography>When there is a new incident,</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          gap={theme.gap.small}
+          mb={theme.gap.large}
+        >
+          <Button level="tertiary" label="Cancel" />
+          <Button level="primary" label="Create" />
         </Stack>
       </Stack>
     </Box>
