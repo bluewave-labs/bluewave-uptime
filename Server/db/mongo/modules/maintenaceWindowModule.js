00726e960809e447bba79735524c38503296d720
Alex Holliday <ajhollid@gmail.com>
2024-07-28 14:37:17 -0700

    add jsdocs

--- Server/db/mongo/modules/maintenaceWindowModule.js
+++ Server/db/mongo/modules/maintenaceWindowModule.js
@@ -1,5 +1,30 @@
 const MaintenanceWindow = require("../../../models/MaintenanceWindow");
 
+/**
+ * Asynchronously creates a new MaintenanceWindow document and saves it to the database.
+ * If the maintenance window is a one-time event, the expiry field is set to the same value as the end field.
+ * @async
+ * @function createMaintenanceWindow
+ * @param {Object} maintenanceWindowData - The data for the new MaintenanceWindow document.
+ * @param {mongoose.Schema.Types.ObjectId} maintenanceWindowData.monitorId - The ID of the monitor.
+ * @param {Boolean} maintenanceWindowData.active - Indicates whether the maintenance window is active.
+ * @param {Boolean} maintenanceWindowData.oneTime - Indicates whether the maintenance window is a one-time event.
+ * @param {Date} maintenanceWindowData.start - The start date and time of the maintenance window.
+ * @param {Date} maintenanceWindowData.end - The end date and time of the maintenance window.
+ * @returns {Promise<MaintenanceWindow>} The saved MaintenanceWindow document.
+ * @throws {Error} If there is an error saving the document.
+ * @example
+ * const maintenanceWindowData = {
+ *   monitorId: 'yourMonitorId',
+ *   active: true,
+ *   oneTime: true,
+ *   start: new Date(),
+ *   end: new Date(),
+ * };
+ * createMaintenanceWindow(maintenanceWindowData)
+ *   .then(maintenanceWindow => console.log(maintenanceWindow))
+ *   .catch(error => console.error(error));
+ */
 const createMaintenanceWindow = async (maintenanceWindowData) => {
   try {
     const maintenanceWindow = new MaintenanceWindow({
@@ -17,6 +42,18 @@ const createMaintenanceWindow = async (maintenanceWindowData) => {
   }
 };
 
+/**
+ * Asynchronously retrieves all MaintenanceWindow documents associated with a specific user ID.
+ * @async
+ * @function getMaintenanceWindowByUserId
+ * @param {String} userId - The ID of the user.
+ * @returns {Promise<Array<MaintenanceWindow>>} An array of MaintenanceWindow documents.
+ * @throws {Error} If there is an error retrieving the documents.
+ * @example
+ * getMaintenanceWindowByUserId('userId')
+ *   .then(maintenanceWindows => console.log(maintenanceWindows))
+ *   .catch(error => console.error(error));
+ */
 const getMaintenanceWindowByUserId = async (userId) => {
   try {
     const maintenanceWindows = await MaintenanceWindow.find({ userId: userId });
