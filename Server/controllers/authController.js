7d3bc7ee8a205af95a0bab8e847290ef39c309fc
Alex Holliday <ajhollid@gmail.com>
2024-07-31 12:55:19 -0700

    Delete notifcations when a user is deleted

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -480,6 +480,7 @@ const deleteUserController = async (req, res, next) => {
           await req.jobQueue.deleteJob(monitor);
           await req.db.deleteChecks(monitor._id);
           await req.db.deleteAlertByMonitorId(monitor._id);
+          await req.db.deleteNotificationsByMonitorId(monitor._id);
         })
       );
 
