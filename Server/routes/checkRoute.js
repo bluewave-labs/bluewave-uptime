67711ffe2c896d129f30abee88a233eb16656e8e
M M <cosmicray23@hotmail.com>
2024-08-06 19:35:49 -0700

    Get rid of test route function.

--- Server/routes/checkRoute.js
+++ Server/routes/checkRoute.js
@@ -25,28 +25,4 @@ router.post(
   checkController.deleteChecks
 );
 
-// Temporary test route
-router.post("/test-notification/:monitorId", async (req, res) => {
-  try {
-    const monitorId = req.params.monitorId;
-    const monitor = await Monitor.findById(monitorId);
-    if (!monitor) {
-      return res.status(404).json({ message: "Monitor not found" });
-    }
 
-    const newCheck = new Check({
-      monitorId,
-      status: false, 
-      responseTime: 100,
-      statusCode: 500,
-      message: "Server error"
-    });
-
-    await newCheck.save();
-    res.status(201).json({ message: "Check created and email notification sent" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-module.exports = router;
