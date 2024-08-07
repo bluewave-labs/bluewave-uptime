7b2311d4689c41201e91547618b70b54ffc69430
M M <cosmicray23@hotmail.com>
2024-08-06 19:36:43 -0700

    Add docs.

--- Server/models/Check.js
+++ Server/models/Check.js
@@ -1,36 +1,78 @@
 const mongoose = require("mongoose");
 const EmailService = require("../service/emailService");
 
+/**
+ * Check Schema for MongoDB collection.
+ * 
+ * Represents a check associated with a monitor, storing information
+ * about the status and response of a particular check event.
+ */
 const CheckSchema = mongoose.Schema(
   {
+    /**
+     * Reference to the associated Monitor document.
+     * 
+     * @type {mongoose.Schema.Types.ObjectId}
+     */
     monitorId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Monitor",
       immutable: true,
     },
+    /**
+     * Status of the check (true for up, false for down).
+     * 
+     * @type {Boolean}
+     */
     status: {
       type: Boolean,
     },
+    /**
+     * Response time of the check in milliseconds.
+     * 
+     * @type {Number}
+     */
     responseTime: {
       type: Number, 
     },
+    /**
+     * HTTP status code received during the check.
+     * 
+     * @type {Number}
+     */
     statusCode: {
       type: Number, 
     },
+    /**
+     * Message or description of the check result.
+     * 
+     * @type {String}
+     */
     message: {
       type: String,
     },
+    /**
+     * Expiry date of the check, auto-calculated to expire after 30 days.
+     * 
+     * @type {Date}
+     */
     expiry: {
       type: Date,
       default: Date.now,
-      expires: 60 * 60 * 24 * 30, 
+      expires: 60 * 60 * 24 * 30, // 30 days
     },
   },
   {
-    timestamps: true,
+    timestamps: true, // Adds createdAt and updatedAt timestamps
   }
 );
 
+/**
+ * Pre-save middleware to handle status change notifications.
+ * 
+ * This middleware checks if the status of the monitor associated
+ * with the check has changed and sends notifications if necessary.
+ */
 CheckSchema.pre("save", async function (next) {
   try {
     const monitor = await mongoose.model("Monitor").findById(this.monitorId);
@@ -82,7 +124,5 @@ CheckSchema.pre("save", async function (next) {
     next();
   }
 });
-
-
 
 module.exports = mongoose.model("Check", CheckSchema);
