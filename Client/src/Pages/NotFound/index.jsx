32a94f04cc61c161100d3d2135aa48bc26f6148f
Alex Holliday <ajhollid@gmail.com>
2024-08-15 15:29:37 -0700

    added 404 image

--- Client/src/Pages/NotFound/index.jsx
+++ Client/src/Pages/NotFound/index.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router";
+import NotFoundSvg from "../../../src/assets/Images/sushi_404.svg";
 
 /**
  * Support for defaultProps will be removed from function components in a future major release
@@ -32,12 +33,14 @@ const DefaultValue = {
  */
 const NotFound = ({ title = DefaultValue.title, desc = DefaultValue.desc }) => {
   const navigate = useNavigate();
-
+  console.log("NOT FOUND");
   return (
     <Stack className="not-found-page" justifyContent="center">
       <Stack gap="20px" alignItems="center">
         <Typography component="h1">{title}</Typography>
         <Typography>{desc}</Typography>
+        <img src={NotFoundSvg} alt="404" style={{ maxHeight: "25rem" }} />
+        <Typography>Oh no! You dropped your sushi!</Typography>
         <Button
           label="Go to the main dashboard"
           level="primary"
