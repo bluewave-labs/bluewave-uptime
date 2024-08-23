931d15c4510d8cec3315a6fc4cc216ad47a2179f
Daniel Cojocea <danielcojocea28@gmail.com>
2024-08-23 14:25:55 -0400

    Added placeholder checks

--- Client/src/Components/Charts/BarChart/index.jsx
+++ Client/src/Components/Charts/BarChart/index.jsx
@@ -6,6 +6,16 @@ import "./index.css";
 const BarChart = ({ checks = [] }) => {
   const theme = useTheme();
 
+  // set responseTime to average if there's only one check
+  if (checks.length === 1) {
+    checks[0] = { ...checks[0], responseTime: 50 };
+  }
+
+  if (checks.length !== 25) {
+    const placeholders = Array(25 - checks.length).fill("placeholder");
+    checks = [...checks, ...placeholders];
+  }
+
   return (
     <Stack
       direction="row"
@@ -18,90 +28,106 @@ const BarChart = ({ checks = [] }) => {
         cursor: "default",
       }}
     >
-      {checks.map((check) => (
-        <Tooltip
-          title={
-            <>
-              <Typography>
-                {formatDate(new Date(check.createdAt), { year: undefined })}
-              </Typography>
-              <Box mt={theme.gap.xs}>
-                <Box
-                  display="inline-block"
-                  width={theme.gap.small}
-                  height={theme.gap.small}
-                  backgroundColor={
-                    check.status
-                      ? theme.label.up.dotColor
-                      : theme.label.down.dotColor
-                  }
-                  sx={{ borderRadius: "50%" }}
-                />
-                <Stack
-                  display="inline-flex"
-                  direction="row"
-                  justifyContent="space-between"
-                  ml={theme.gap.xs}
-                  gap={theme.gap.large}
-                >
-                  <Typography component="span" sx={{ opacity: 0.8 }}>
-                    Response Time
-                  </Typography>
-                  <Typography component="span">
-                    {check.originalResponseTime}
-                    <Typography component="span" sx={{ opacity: 0.8 }}>
-                      {" "}
-                      ms
-                    </Typography>
-                  </Typography>
-                </Stack>
-              </Box>
-            </>
-          }
-          placement="top"
-          key={`check-${check?._id}`}
-          slotProps={{
-            popper: {
-              className: "bar-tooltip",
-              modifiers: [
-                {
-                  name: "offset",
-                  options: {
-                    offset: [0, -10],
-                  },
-                },
-              ],
-            },
-          }}
-        >
+      {checks.map((check, index) =>
+        check === "placeholder" ? (
           <Box
+            key={`${check}-${index}`}
             position="relative"
             width="9px"
             height="100%"
-            backgroundColor={
-              check.status ? theme.label.up.bgColor : theme.label.down.bgColor
-            }
+            backgroundColor={theme.palette.otherColors.fillGray}
             sx={{
               borderRadius: "3px",
             }}
+          />
+        ) : (
+          <Tooltip
+            title={
+              <>
+                <Typography>
+                  {formatDate(new Date(check.createdAt), { year: undefined })}
+                </Typography>
+                <Box mt={theme.gap.xs}>
+                  <Box
+                    display="inline-block"
+                    width={theme.gap.small}
+                    height={theme.gap.small}
+                    backgroundColor={
+                      check.status
+                        ? theme.label.up.dotColor
+                        : theme.label.down.dotColor
+                    }
+                    sx={{ borderRadius: "50%" }}
+                  />
+                  <Stack
+                    display="inline-flex"
+                    direction="row"
+                    justifyContent="space-between"
+                    ml={theme.gap.xs}
+                    gap={theme.gap.large}
+                  >
+                    <Typography component="span" sx={{ opacity: 0.8 }}>
+                      Response Time
+                    </Typography>
+                    <Typography component="span">
+                      {check.originalResponseTime}
+                      <Typography component="span" sx={{ opacity: 0.8 }}>
+                        {" "}
+                        ms
+                      </Typography>
+                    </Typography>
+                  </Stack>
+                </Box>
+              </>
+            }
+            placement="top"
+            key={`check-${check?._id}`}
+            slotProps={{
+              popper: {
+                className: "bar-tooltip",
+                modifiers: [
+                  {
+                    name: "offset",
+                    options: {
+                      offset: [0, -10],
+                    },
+                  },
+                ],
+              },
+            }}
           >
             <Box
-              position="absolute"
-              bottom={0}
-              width="100%"
-              height={`${check.responseTime}%`}
+              position="relative"
+              width="9px"
+              height="100%"
               backgroundColor={
-                check.status
-                  ? theme.label.up.dotColor
-                  : theme.label.down.dotColor
+                check.status ? theme.label.up.bgColor : theme.label.down.bgColor
               }
               sx={{
                 borderRadius: "3px",
+                "&:hover > .MuiBox-root": {
+                    filter: "brightness(0.8)",
+                  },
               }}
-            />
-          </Box>
-        </Tooltip>
-      ))}
+            >
+              <Box
+                position="absolute"
+                bottom={0}
+                width="100%"
+                height={`${check.responseTime}%`}
+                backgroundColor={
+                  check.status
+                    ? theme.label.up.dotColor
+                    : theme.label.down.dotColor
+                }
+                sx={{
+                  borderRadius: "3px",
+                }}
+              />
+            </Box>
+          </Tooltip>
+        )
+      )}
     </Stack>
   );
 };
