d556de05b77bd220745d3667ee5de88cffb1c40a
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-08-28 23:25:26 +0330

    autocomplete implementation

--- Client/src/Components/Inputs/Autocomplete/index.jsx
+++ Client/src/Components/Inputs/Autocomplete/index.jsx
@@ -11,13 +11,9 @@ import match from "autosuggest-highlight/match";
  * @component
  * @param {Object} props - The component props.
  * @param {string} props.id - The ID of the autocomplete field.
- * @param {string} props.label - The label of the autocomplete field.
- * @param {React.Element} props.icon - The icon element to display.
  * @param {string} props.type - The type of the input field (text or number).
  * @param {Array} props.options - The options for the autocomplete field.
- * @param {string} props.error - The error message to display.
  * @param {string} props.placeholder - The placeholder text for the input field.
- * @param {boolean} props.isRequired - Indicates if the field is required.
  * @param {function} props.onChange - The function to handle onChange event.
  * @param {string} props.value - The value of the input field.
  * @param {boolean} props.disabled - Indicates if the field is disabled.
@@ -27,95 +23,67 @@ import match from "autosuggest-highlight/match";
 
 const AutoCompleteField = ({
   id,
-  label,
-  icon,
   type,
-  options,
-  error,
-  placeholder,
-  isRequired,
-  onChange,
-  value,
+  options = optionsList,
+  placeholder = "Search",
   disabled,
   setWidth,
 }) => {
+  const [value, setValue] = React.useState();
+  const [inputValue, setInputValue] = React.useState("");
+
   return (
-    <Stack className="auto-complete-field" gap={1}>
-      {label && (
-        <Typography
-          style={{
-            color: "var(--env-var-color-5)",
-            fontSize: "var(--env-var-font-size-medium)",
-            fontWeight: 600,
-          }}
+    <Autocomplete
+      className="auto-complete-field"
+      sx={{
+        width: "fit-content",
+        padding: "8px",
+        borderRadius: "var( --env-var-radius-1)",
+        border: "1px solid #D0D5DD",
+        alignItems: "center",
+      }}
+      id={id}
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      inputValue={inputValue}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      options={options}
+      getOptionLabel={(option) => option.title}
+      renderInput={(params) => (
+        <div
+          ref={params.InputProps.ref}
+          style={{ minWidth: 250, maxWidth: 320, width: setWidth }}
         >
-          {label}
-        </Typography>
+          <input
+            type={type}
+            {...params.inputProps}
+            placeholder={placeholder}
+            disabled={disabled}
+          />
+        </div>
       )}
-      <Stack
-        sx={{
-          width: "fit-content",
-          padding: "8px",
-          borderRadius: "var( --env-var-radius-1)",
-          border: isRequired ? "1px solid #d32f2f" : "1px solid #D0D5DD",
-          alignItems: "center",
-        }}
-        gap={1}
-        direction="row"
-        alignItems="center"
-      >
-        {icon && icon}
+      renderOption={(props, option, { inputValue }) => {
+        const { key, ...optionProps } = props;
+        const matches = match(option.title, inputValue, {
+          insideWords: true,
+        });
+        const parts = parse(option.title, matches);
 
-        <Autocomplete
-          id={id}
-          options={options}
-          getOptionLabel={(option) => option.title}
-          renderInput={(params) => (
-            <div
-              ref={params.InputProps.ref}
-              style={{ minWidth: 250, maxWidth: 320, width: setWidth }}
-            >
-              <input
-                type={type}
-                {...params.inputProps}
-                placeholder={placeholder}
-                onChange={onChange}
-                disabled={disabled}
-                value={value}
-              />
+        return (
+          <li key={key} {...optionProps}>
+            <div>
+              {parts.map((part, index) => (
+                <span key={index}>{part.text}</span>
+              ))}
             </div>
-          )}
-          renderOption={(props, option, { inputValue }) => {
-            const { key, ...optionProps } = props;
-            const matches = match(option.title, inputValue, {
-              insideWords: true,
-            });
-            const parts = parse(option.title, matches);
-
-            return (
-              <li key={key} {...optionProps}>
-                <div>
-                  {parts.map((part, index) => (
-                    <span key={index}>{part.text}</span>
-                  ))}
-                </div>
-              </li>
-            );
-          }}
-        />
-      </Stack>
-      {error && (
-        <Typography
-          style={{
-            color: "var(--env-var-color-34)",
-            fontSize: "var(--env-var-font-size-medium)",
-            marginLeft: "var(--env-var-spacing-1)",
-          }}
-        >
-          {error}
-        </Typography>
-      )}
-    </Stack>
+          </li>
+        );
+      }}
+    />
   );
 };
 
@@ -123,17 +91,141 @@ AutoCompleteField.displayName = "AutoCompleteField";
 
 AutoCompleteField.propTypes = {
   id: PropTypes.string,
-  label: PropTypes.string,
-  icon: PropTypes.element,
   type: PropTypes.oneOf(["text", "number"]),
   options: PropTypes.array,
-  error: PropTypes.string,
   placeholder: PropTypes.string,
-  isRequired: PropTypes.bool,
   onChange: PropTypes.func,
   value: PropTypes.string,
   disabled: PropTypes.bool,
   setWidth: PropTypes.string,
 };
+
+const optionsList = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
+  { title: "The Good, the Bad and the Ugly", year: 1966 },
+  { title: "Fight Club", year: 1999 },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001,
+  },
+  {
+    title: "Star Wars: Episode V - The Empire Strikes Back",
+    year: 1980,
+  },
+  { title: "Forrest Gump", year: 1994 },
+  { title: "Inception", year: 2010 },
+  {
+    title: "The Lord of the Rings: The Two Towers",
+    year: 2002,
+  },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: "Goodfellas", year: 1990 },
+  { title: "The Matrix", year: 1999 },
+  { title: "Seven Samurai", year: 1954 },
+  {
+    title: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+  },
+  { title: "City of God", year: 2002 },
+  { title: "Se7en", year: 1995 },
+  { title: "The Silence of the Lambs", year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: "Life Is Beautiful", year: 1997 },
+  { title: "The Usual Suspects", year: 1995 },
+  { title: "Léon: The Professional", year: 1994 },
+  { title: "Spirited Away", year: 2001 },
+  { title: "Saving Private Ryan", year: 1998 },
+  { title: "Once Upon a Time in the West", year: 1968 },
+  { title: "American History X", year: 1998 },
+  { title: "Interstellar", year: 2014 },
+  { title: "Casablanca", year: 1942 },
+  { title: "City Lights", year: 1931 },
+  { title: "Psycho", year: 1960 },
+  { title: "The Green Mile", year: 1999 },
+  { title: "The Intouchables", year: 2011 },
+  { title: "Modern Times", year: 1936 },
+  { title: "Raiders of the Lost Ark", year: 1981 },
+  { title: "Rear Window", year: 1954 },
+  { title: "The Pianist", year: 2002 },
+  { title: "The Departed", year: 2006 },
+  { title: "Terminator 2: Judgment Day", year: 1991 },
+  { title: "Back to the Future", year: 1985 },
+  { title: "Whiplash", year: 2014 },
+  { title: "Gladiator", year: 2000 },
+  { title: "Memento", year: 2000 },
+  { title: "The Prestige", year: 2006 },
+  { title: "The Lion King", year: 1994 },
+  { title: "Apocalypse Now", year: 1979 },
+  { title: "Alien", year: 1979 },
+  { title: "Sunset Boulevard", year: 1950 },
+  {
+    title:
+      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    year: 1964,
+  },
+  { title: "The Great Dictator", year: 1940 },
+  { title: "Cinema Paradiso", year: 1988 },
+  { title: "The Lives of Others", year: 2006 },
+  { title: "Grave of the Fireflies", year: 1988 },
+  { title: "Paths of Glory", year: 1957 },
+  { title: "Django Unchained", year: 2012 },
+  { title: "The Shining", year: 1980 },
+  { title: "WALL·E", year: 2008 },
+  { title: "American Beauty", year: 1999 },
+  { title: "The Dark Knight Rises", year: 2012 },
+  { title: "Princess Mononoke", year: 1997 },
+  { title: "Aliens", year: 1986 },
+  { title: "Oldboy", year: 2003 },
+  { title: "Once Upon a Time in America", year: 1984 },
+  { title: "Witness for the Prosecution", year: 1957 },
+  { title: "Das Boot", year: 1981 },
+  { title: "Citizen Kane", year: 1941 },
+  { title: "North by Northwest", year: 1959 },
+  { title: "Vertigo", year: 1958 },
+  {
+    title: "Star Wars: Episode VI - Return of the Jedi",
+    year: 1983,
+  },
+  { title: "Reservoir Dogs", year: 1992 },
+  { title: "Braveheart", year: 1995 },
+  { title: "M", year: 1931 },
+  { title: "Requiem for a Dream", year: 2000 },
+  { title: "Amélie", year: 2001 },
+  { title: "A Clockwork Orange", year: 1971 },
+  { title: "Like Stars on Earth", year: 2007 },
+  { title: "Taxi Driver", year: 1976 },
+  { title: "Lawrence of Arabia", year: 1962 },
+  { title: "Double Indemnity", year: 1944 },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    year: 2004,
+  },
+  { title: "Amadeus", year: 1984 },
+  { title: "To Kill a Mockingbird", year: 1962 },
+  { title: "Toy Story 3", year: 2010 },
+  { title: "Logan", year: 2017 },
+  { title: "Full Metal Jacket", year: 1987 },
+  { title: "Dangal", year: 2016 },
+  { title: "The Sting", year: 1973 },
+  { title: "2001: A Space Odyssey", year: 1968 },
+  { title: "Singin' in the Rain", year: 1952 },
+  { title: "Toy Story", year: 1995 },
+  { title: "Bicycle Thieves", year: 1948 },
+  { title: "The Kid", year: 1921 },
+  { title: "Inglourious Basterds", year: 2009 },
+  { title: "Snatch", year: 2000 },
+  { title: "3 Idiots", year: 2009 },
+  { title: "Monty Python and the Holy Grail", year: 1975 },
+];
 
 export default AutoCompleteField;
