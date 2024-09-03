b3d33a5ff68451ea646bcea5d0bea10e4e703741
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-09-03 21:38:08 +0330

    autocomplete

--- Client/src/Components/Inputs/Autocomplete/index.jsx
+++ Client/src/Components/Inputs/Autocomplete/index.jsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Stack, TextField, Typography } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 import React from "react";
 import PropTypes from "prop-types";
 import { useTheme } from "@emotion/react";
