ee30f44062d00a0a13043077f19ee7286a54c3c6
Daniel Cojocea <danielcojocea28@gmail.com>
2024-09-18 19:50:01 -0400

    Added basic layout

--- Client/src/Pages/Maintenance/CreateMaintenance/index.jsx
+++ Client/src/Pages/Maintenance/CreateMaintenance/index.jsx
@@ -1,22 +1,74 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { useState } from "react";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
-import { maintenanceWindowValidation } from "../../../Validation/validation";
-import { logger } from "../../../Utils/Logger";
-import { createToast } from "../../../Utils/toastUtils";
 import Select from "../../../Components/Inputs/Select";
-import dayjs from "dayjs";
 import Field from "../../../Components/Inputs/Field";
+import Breadcrumbs from "../../../Components/Breadcrumbs";
 import "./index.css";
 
 const CreateMaintenance = () => {
   const theme = useTheme();
 
-  return <Box className="create-maintenance"></Box>;
+  return (
+    <Box className="create-maintenance">
+      <Breadcrumbs
+        list={[
+          { name: "mainteanance", path: "/maintenance" },
+          { name: "create", path: `/maintenance/create` },
+        ]}
+      />
+      <Stack
+        component="form"
+        noValidate
+        spellCheck="false"
+        gap={theme.spacing(12)}
+        mt={theme.spacing(6)}
+      >
+        <Typography component="h1" variant="h1">
+          <Typography component="span" fontSize="inherit">
+            Create a{" "}
+          </Typography>
+          <Typography
+            component="span"
+            variant="h2"
+            fontSize="inherit"
+            fontWeight="inherit"
+          >
+            maintenance{" "}
+          </Typography>
+          <Typography component="span" fontSize="inherit">
+            window
+          </Typography>
+        </Typography>
+        <Box
+          width="100%"
+          p={theme.spacing(15)}
+          border={1}
+          borderColor={theme.palette.border.light}
+          borderRadius={theme.shape.borderRadius}
+          backgroundColor={theme.palette.background.main}
+        ></Box>
+        <Box ml="auto" display="inline-block">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => console.log("disabled")}
+            sx={{ mr: theme.spacing(6) }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => console.log("disabled")}
+            disabled={false}
+          >
+            Create window
+          </Button>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default CreateMaintenance;
