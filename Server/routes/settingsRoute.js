31509247de097149337f83288427e888bb4c92c0
Alex Holliday <ajhollid@gmail.com>
2024-09-30 15:20:10 +0800

    removed console logs, removed permission check for getting settings

--- Server/routes/settingsRoute.js
+++ Server/routes/settingsRoute.js
@@ -3,7 +3,7 @@ const settingsController = require("../controllers/settingsController");
 const { isAllowed } = require("../middleware/isAllowed");
 const Monitor = require("../models/Monitor");
 
-router.get("/", isAllowed(["superadmin"]), settingsController.getAppSettings);
+router.get("/", settingsController.getAppSettings);
 router.put(
   "/",
   isAllowed(["superadmin"]),
