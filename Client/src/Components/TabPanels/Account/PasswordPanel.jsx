077d26e6a5c89f7a73d348f165eccd0f4d4acebc
om-3004 <omthakkar3004@gmail.com>
2024-10-11 16:12:54 +0530

    fix: removed an unnecessary hidden input field to make current password input consistent

--- Client/src/Components/TabPanels/Account/PasswordPanel.jsx
+++ Client/src/Components/TabPanels/Account/PasswordPanel.jsx
@@ -117,14 +117,6 @@ const PasswordPanel = () => {
             <Typography component="h1">Current password</Typography>
           </Box>
           <Field
-            type="text"
-            id="hidden-username"
-            name="username"
-            autoComplete="username"
-            hidden={true}
-            value=""
-          />
-          <Field
             type="password"
             id="edit-current-password"
             placeholder="Enter your current password"
