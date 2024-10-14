f34b8466cde1e07d30f52ae0995f8b18074db1cf
Alex Holliday <ajhollid@gmail.com>
2024-10-14 13:38:53 +0800

    Add tests for updateAppSettings

--- Server/tests/controllers/settingsController.test.js
+++ Server/tests/controllers/settingsController.test.js
@@ -47,3 +47,59 @@ describe("Settings Controller - getAppSettings", () => {
     });
   });
 });
+
+describe("Settings Controller - updateAppSettings", () => {
+  beforeEach(() => {
+    req = {
+      headers: {},
+      params: {},
+      body: {},
+      db: {
+        updateAppSettings: sinon.stub(),
+      },
+      settingsService: {
+        reloadSettings: sinon.stub(),
+      },
+    };
+    res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    next = sinon.stub();
+    handleError = sinon.stub();
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("should reject with an error if body validation fails", async () => {
+    req.body = { invalid: 1 };
+    await updateAppSettings(req, res, next);
+    expect(next.firstCall.args[0]).to.be.an("error");
+    expect(next.firstCall.args[0].status).to.equal(422);
+  });
+  it("should reject with an error if updateAppSettings throws an error", async () => {
+    req.db.updateAppSettings.throws(new Error("updateAppSettings error"));
+    await updateAppSettings(req, res, next);
+    expect(next.firstCall.args[0]).to.be.an("error");
+    expect(next.firstCall.args[0].message).to.equal("updateAppSettings error");
+  });
+  it("should reject with an error if reloadSettings throws an error", async () => {
+    req.settingsService.reloadSettings.throws(
+      new Error("reloadSettings error")
+    );
+    await updateAppSettings(req, res, next);
+    expect(next.firstCall.args[0]).to.be.an("error");
+    expect(next.firstCall.args[0].message).to.equal("reloadSettings error");
+  });
+  it("should return a success message and data if updateAppSettings is successful", async () => {
+    const data = { data: "settings" };
+    req.settingsService.reloadSettings.returns(data);
+    await updateAppSettings(req, res, next);
+    expect(res.status.firstCall.args[0]).to.equal(200);
+    expect(res.json.firstCall.args[0]).to.deep.equal({
+      success: true,
+      msg: successMessages.UPDATE_APP_SETTINGS,
+      data,
+    });
+  });
+});
