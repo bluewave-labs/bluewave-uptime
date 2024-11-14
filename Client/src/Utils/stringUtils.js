407e5a871124e5ee7f087cbf30ef05c91c404902
Shemy Gan <jennifergan26@gmail.com>
2024-11-13 20:16:35 -0500

    - Add JSDoc and use css varialble for pixels

--- Client/src/Utils/stringUtils.js
+++ Client/src/Utils/stringUtils.js
@@ -1,3 +1,8 @@
+/**
+ * Helper function to get first letter capitalized string
+ * @param {string} str String whose first letter is to be capitalized
+ * @returns A string with first letter capitalized
+ */
 export const capitalizeFirstLetter = (str) =>{
-  return  str.charAt(0).toUpperCase() + str.slice(1);
+  return  str?.charAt(0).toUpperCase() + str.slice(1);
 }
