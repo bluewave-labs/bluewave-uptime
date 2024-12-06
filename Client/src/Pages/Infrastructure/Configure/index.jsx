837594913ac41068f48d6fb808818e9bedaa40d9
Peter Carl Pardo <peterpardo123@gmail.com>
2024-12-06 14:45:43 +0800

    feat: add configure insfrastructure monitor route and component

---
+++ Client/src/Pages/Infrastructure/Configure/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConfigureInfrastructureMonitor = () => {
+	return <div>Configure Infrastructure Monitor Page</div>;
+};
+
+export default ConfigureInfrastructureMonitor;
