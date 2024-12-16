e30b64bed3f73f579e8eaf2353008c11b23dd599
Shalini M <shalinim@TOSHIBA-TSIP.COM>
2024-12-16 17:08:27 +0530

    Changed the avatar color to checkmate blue color

--- Client/src/Components/Avatar/index.jsx
+++ Client/src/Components/Avatar/index.jsx
@@ -9,19 +9,8 @@ import { useEffect, useState } from "react";
  * @returns {string}
  */
 const stringToColor = (string) => {
-	let hash = 0;
-	let i;
-	for (i = 0; i < string.length; i += 1) {
-		hash = string.charCodeAt(i) + ((hash << 5) - hash);
-	}
 
-	let color = "#";
-	for (i = 0; i < 3; i += 1) {
-		const value = (hash >> (i * 8)) & 0xff;
-		color += `00${value.toString(16)}`.slice(-2);
-	}
-
-	return color;
+	return "#156EEF";
 };
 
 /**
