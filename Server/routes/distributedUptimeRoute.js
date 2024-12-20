39192d3df960ddb591d1cc873e5d578dcb041959
Alex Holliday <ajhollid@gmail.com>
2024-12-20 10:32:06 -0800

    Add distributed uptime route

--- Server/routes/distributedUptimeRoute.js
+++ Server/routes/distributedUptimeRoute.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { resultsCallback } from "../controllers/distributedUptimeController.js";
 const router = Router();
 
-router.post("/distributed-uptime/results", resultsCallback);
+router.post("/results", resultsCallback);
 
 export default router;
