61b570d7d9090e2ca41b29f43053bd2174e47a45
Alex Holliday <ajhollid@gmail.com>
2024-12-20 10:31:48 -0800

    Add controller for distributed uptime

--- Server/controllers/distributedUptimeController.js
+++ Server/controllers/distributedUptimeController.js
@@ -5,7 +5,7 @@ import { handleValidationError, handleError } from "./controllerUtils.js";
 const resultsCallback = async (req, res, next) => {
 	try {
 		console.log(req.body);
-		res.status(200);
+		res.status(200).json({ message: "OK" });
 	} catch (error) {
 		throw handleError(error, SERVICE_NAME, "resultsCallback");
 	}
